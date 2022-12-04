a8910f34bc0b93c530db73f1e507743920419cf2
xudong721 <110542041+xudong721@users.noreply.github.com>
2022-12-04 23:23:20 +0100

    setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+    name="snowflake",
+    version="0.1",
+    author="li",
+    author_email="562824592xd@gmail.com",
+    packages=find_packages(),
+    install_requires=["numpy","turtles"]
+)
